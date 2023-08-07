d8204c8fe986118fafdb47c80d780cfcea64fd28
capmec <capmec@gmail.com>
2023-08-07 14:23:13 +0200

    authentication done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+//app/page.tsx
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Threads</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
