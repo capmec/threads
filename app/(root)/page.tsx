e981230bd6b87946de09a2664baa05cf9a3f9e8e
capmec <capmec@gmail.com>
2023-08-25 09:19:49 +0200

    cards structures for posts and comments

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,19 +10,25 @@ export default async function Home() {
         <>
             <h1 className='head-text text-left'>Home</h1>
             <section className='mt-9 flex flex-col gap-10'>
-                {result.posts.map((post) => (
-                    <ThreadCard
-                        key={post._id}
-                        id={post._id}
-                        currentUserId={user?.id || ''}
-                        parentId={post.parentId}
-                        content={post.text}
-                        author={post.author}
-                        community={post.community}
-                        createdAt={post.createdAt}
-                        comments={post.children}
-                    />
-                ))}
+                {result.posts.length === 0 ? (
+                    <p className='no-result'>No threads found</p>
+                ) : (
+                    <div>
+                        {result.posts.map((post) => (
+                            <ThreadCard
+                                key={post._id}
+                                id={post._id}
+                                currentUserId={user?.id || ''}
+                                parentId={post.parentId}
+                                content={post.text}
+                                author={post.author}
+                                community={post.community}
+                                createdAt={post.createdAt}
+                                comments={post.children}
+                            />
+                        ))}
+                    </div>
+                )}
             </section>
         </>
     )
