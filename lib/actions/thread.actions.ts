6e8ec646e21650f2d86045bab61b310cef22bf88
capmec <capmec@gmail.com>
2023-08-26 16:13:25 +0200

    user profile page done

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -148,3 +148,29 @@ export async function addCommentToThread(
         throw new Error(`Failed to add comment to thread: ${error.message}`)
     }
 }
+
+export async function fetchUserPosts(userId: string) {
+    try {
+        connectToDB()
+        //TODO: Populate community
+
+        //Find all threads authored by the user with th given userID
+        const threads = await User.findOne({ id: userId }).populate({
+            path: 'threads',
+            model: Thread,
+            populate: {
+                path: 'children',
+                model: Thread,
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: 'name image id',
+                },
+            },
+        })
+
+        return threads
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user posts: ${error.message}`)
+    }
+}
